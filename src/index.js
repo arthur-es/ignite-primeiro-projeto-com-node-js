1c75483a6d6810dd89e9831b8d8e0a18a5fec905
Arthur Castro <arthur@hyerdev.com>
2021-03-13 13:23:19 -0300

    feat: get statement and make deposit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfAccountWithGivenCPFExists(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customerFound = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customerFound) {
+    return res
+      .status(404)
+      .json({ error: `Customer with CPF ${cpf} do not exists.` });
+  }
+
+  req.customer = customerFound;
+
+  return next();
+}
+
 app.post("/account", (req, res) => {
   const { name, cpf } = req.body;
 
@@ -28,6 +44,28 @@ app.post("/account", (req, res) => {
   });
 
   return res.status(201).json(customers);
+});
+
+app.get("/statement", verifyIfAccountWithGivenCPFExists, (req, res) => {
+  const { customer } = req;
+
+  return res.status(200).json(customer);
+});
+
+app.post("/deposit", verifyIfAccountWithGivenCPFExists, (req, res) => {
+  const { customer } = req;
+  const { description, amount } = req.body;
+
+  const statementOperation = {
+    type: "credit",
+    description,
+    amount,
+    created_at: new Date(),
+  };
+
+  customer.statement.push(statementOperation);
+
+  return res.status(201).send();
 });
 
 app.listen(3333);
